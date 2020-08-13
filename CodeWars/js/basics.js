0f49bc7f9cc245557f5f81dd7710ddb8d52eb7a3
Cruzanio <cruzanio.villarreal@gmail.com>
2020-08-13 13:22:29 -0500

    Array Basics

---
+++ CodeWars/js/basics.js
@@ -0,0 +1,56 @@
+"use strict"
+
+function helloWorld() {
+    var helloWorldPrint = 'Hello World'
+    console.log(helloWorldPrint)
+}
+helloWorld()
+
+var v1 = 50
+var v2 = 100
+
+
+function equal1() {
+    var localTotal = v1 + v2
+    console.log(localTotal)
+}
+equal1()
+
+function equal2() {
+    var localTotal = v1 - v2
+    console.log(localTotal)
+}
+equal2()
+
+function equal3() {
+    var localTotal = v1 * v2
+    console.log(localTotal)
+}
+equal3()
+
+function equal4() {
+    var localTotal = v1 / v2
+    console.log(localTotal)
+}
+equal4()
+
+function equal5() {
+    var localTotal = v1 % v2
+    console.log(localTotal)
+}
+equal5()
+
+
+var l1 = 'H'
+var l2 = 'e'
+var l3 = 'l'
+var l4 = 'l'
+var l5 = 'o'
+var l6 = " World"
+
+function conCat() {
+    var fCat = l1 + l2 + l3 + l4 + l5 + l6
+    console.log(fCat)
+}
+conCat()
+
