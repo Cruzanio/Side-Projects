20ae5eb23444b841a8e9d9cc6df58c1c85d252ac
Cruzanio <cruzanio.villarreal@gmail.com>
2020-08-20 15:47:51 -0500

    Function Practice

--- CodeWars/js/basics.js
+++ CodeWars/js/basics.js
@@ -94,3 +94,17 @@ function TrueOrFalse(val) {
 }
 TrueOrFalse(val1)
 
+function saleHotdogs(n){
+
+    if (n < 5) {
+        var price = 100 * n
+        return price
+    } else if (n >= 5 && n < 10) {
+        price = 95 * n
+        return price
+    } else {
+        price = 90 * n
+        return price
+    }
+}
+
