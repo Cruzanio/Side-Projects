41f2c1a5d65035c7d3e007dc087a60d3e6d43bc0
Cruzanio <cruzanio.villarreal@gmail.com>
2020-08-24 20:55:18 -0500

    For Loop Practice

--- CodeWars/js/doWhileWhile.js
+++ CodeWars/js/doWhileWhile.js
@@ -1,1 +1,15 @@
 "use strict";
+
+//Alternates adding * before or after the string
+
+function padIt(str,n){
+    while(n>0){
+        if(n%2 === 0) {
+            str = str + "*";
+        }else{
+            str = "*" + str;
+        }
+        n --;
+    }
+    return str;
+}
