fa580f1025f6e1a3100dfd4018d5ebb248edd235
Cruzanio <cruzanio.villarreal@gmail.com>
2020-09-09 16:47:15 -0500

    Successful Data Requests

--- Restaurant API/restaurant.js
+++ Restaurant API/restaurant.js
@@ -1,2 +1,25 @@
-"use strict";
+(function() {
+    "use strict";
+    $().ready(function () {
+        var postLoad = function () {
+            $.ajax("https://frest.glitch.me/restaurants").done(function (data, status, jqXhr) {
+                console.log("Data returned from server:" + data);
+                var innerHTML = ''
+                data.forEach(function (item) {
+                    innerHTML += "<h1>" + item["name"] +
+                        "</h1><p>" + item["cuisine_type"] +
+                        // "</p><p>" + item['operating_hours'].forEach(function() {
+                        //     for (var i = 0; i < )
+                        // }) +
+                        "</p><br>\n"
+                });
+                $('#open').html(innerHTML)
+            });
+        }
+        postLoad()
 
+        // $('#refresh').click(function () {
+        //     postLoad()
+        // })
+    });
+})();
