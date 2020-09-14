2a5122167805bb806e15784c5ed06b7f6b4033aa
cruzaniovillarreal <cruzanio.villarreal@gmail.com>
2020-09-14 17:32:12 -0500

    test

--- Mini Labs/js/fizzbuzz.js
+++ Mini Labs/js/fizzbuzz.js
@@ -12,3 +12,68 @@ function fizzbuzz(num) {
     }
 }
 fizzbuzz(30)
+
+    // [
+    // {
+    //     "title": "Hammer",
+    //     "quantity": 25,
+    //     "categories": [
+    //         "tool"
+    //     ],
+    //     "price": 20
+    // }, {
+    //     "title": "Drill",
+    //     "quantity": 5,
+    //     "categories": [
+    //         "tool",
+    //         "powered"
+    //     ],
+    //     "price": 100
+    // }, {
+    //     "title": "Mower",
+    //     "quantity": 5,
+    //     "categories": [
+    //         "tool",
+    //         "gas",
+    //         "outdoor"
+    //     ],
+    //     "price": 200
+    // }, {
+    //     "title": "Screwdriver",
+    //     "quantity": 25,
+    //     "categories": [
+    //         "tool"
+    //     ],
+    //     "price": 10
+    // },    {
+    //     "title": "Computer",
+    //     "quantity": 2,
+    //     "categories": [
+    //         "tool"
+    //     ],
+    //     "price": 699
+    // }, {
+    //     "title": "Pencil",
+    //     "quantity": 500,
+    //     "categories": [
+    //         "tool"
+    //     ],
+    //     "price": 1
+    // }, {
+    //     "title": "Tesla",
+    //     "quantity": 5,
+    //     "categories": [
+    //         "electric",
+    //         "car",
+    //         "outdoor"
+    //     ],
+    //     "price": 50000
+    // }, {
+    //     "title": "Blue Jeans",
+    //     "quantity": 27,
+    //     "categories": [
+    //         "clothing"
+    //     ],
+    //     "price": 27
+    // }
+    // ]
