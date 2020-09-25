faaabd890054a2f09eefa8eec31863c94243448b
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-09-24 19:59:41 -0500

    formula for calculating input number

--- Interactives/narc.js
+++ Interactives/narc.js
@@ -3,13 +3,19 @@
 
         $('#run').click(function () {
             let userEntry = $('#input').val()
-            console.log(userEntry)
             let numberSplit = userEntry.split("")
+            console.log(numberSplit)
+            function arraytoNum(array) {
+                let addTo = ''
+                for (let element of array) {
+                    addTo += ` (${element} ^ ${userEntry.length}) +`
+                } return addTo.slice(0, -1)
+            }
             $('#outputConsole').append(`
                   <div>Your Number: ${userEntry}</div>
                    <div>Number of digits: ${numberSplit.length}<br>
-                   <div>Digits to add: ${function () {${numberSplit}.}}</div></div>
-                   <div></div>        
+                   <div>Calculating: ${arraytoNum(numberSplit)}</div></div>
+                   <div>Result = </div>        
                 `)
         })
 
