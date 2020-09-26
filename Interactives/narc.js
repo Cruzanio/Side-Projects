c7f4c8313d031d7788c9f29ff46a7e264eb2b785
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-09-26 14:29:14 -0500

    working calculations

--- Interactives/narc.js
+++ Interactives/narc.js
@@ -4,21 +4,33 @@
         $('#run').click(function () {
             let userEntry = $('#input').val()
             let numberSplit = userEntry.split("")
-            console.log(numberSplit)
+            let calculate = 0
+            let yesOrNo = ""
+
             function arraytoNum(array) {
                 let addTo = ''
                 for (let element of array) {
                     addTo += ` (${element} ^ ${userEntry.length}) +`
-                } return addTo.slice(0, -1)
+                    calculate += Math.pow(element, userEntry.length)
+                }
+                return addTo.slice(0, -1)
             }
+
+            arraytoNum(numberSplit)
+            if (calculate === parseInt(userEntry)) {
+                yesOrNo = 'Yes'
+            } else {
+                yesOrNo = 'No'
+            }
+
+            $('#outputConsole').html("")
             $('#outputConsole').append(`
                   <div>Your Number: ${userEntry}</div>
                    <div>Number of digits: ${numberSplit.length}<br>
                    <div>Calculating: ${arraytoNum(numberSplit)}</div></div>
-                   <div>Result = </div>        
+                   <div>Result = ${calculate}</div>      
+                   <div>Narcissistic? ${yesOrNo}</div>  
                 `)
         })
-
-
     })
 })(jQuery);
