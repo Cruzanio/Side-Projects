1dc175ad17c3e45a4412742454c3f6fac014bd62
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-09-27 09:15:40 -0500

    Yes no display

--- Interactives/narc.js
+++ Interactives/narc.js
@@ -2,34 +2,36 @@
     $(document).ready(function () {
 
         $('#run').click(function () {
+            $('.yesNo').css('background', 'transparent')
             let userEntry = $('#input').val()
             let numberSplit = userEntry.split("")
             let calculate = 0
-            let yesOrNo = ""
 
             function arraytoNum(array) {
                 let addTo = ''
+                calculate = 0
                 for (let element of array) {
                     addTo += ` (${element} ^ ${userEntry.length}) +`
                     calculate += Math.pow(element, userEntry.length)
                 }
                 return addTo.slice(0, -1)
             }
-
             arraytoNum(numberSplit)
             if (calculate === parseInt(userEntry)) {
                 yesOrNo = 'Yes'
+                $('#yes').css('color', 'black')
+                $('#yes').css('background', 'limegreen')
             } else {
                 yesOrNo = 'No'
+                $('#no').css('background', 'red')
             }
 
             $('#outputConsole').html("")
             $('#outputConsole').append(`
-                  <div>Your Number: ${userEntry}</div>
-                   <div>Number of digits: ${numberSplit.length}<br>
+                  <div>Your Number (<em>n</em>): ${userEntry}</div>
+                   <div>Number of digits (<em>m</em>): ${numberSplit.length}<br>
                    <div>Calculating: ${arraytoNum(numberSplit)}</div></div>
-                   <div>Result = ${calculate}</div>      
-                   <div>Narcissistic? ${yesOrNo}</div>  
+                   <div>Result = ${calculate}</div>
                 `)
         })
     })
