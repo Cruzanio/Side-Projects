0d2916154b7312d5d52a1b66c406a8866053dda2
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-09-27 11:52:48 -0500

    Animations

--- Interactives/narc.js
+++ Interactives/narc.js
@@ -1,7 +1,9 @@
 (function ($) {
     $(document).ready(function () {
 
+        $('.yesNo').addClass('d-none')
         $('#run').click(function () {
+            $('.yesNo').addClass('d-none')
             $('.yesNo').css('background', 'transparent')
             let userEntry = $('#input').val()
             let numberSplit = userEntry.split("")
@@ -16,23 +18,31 @@
                 }
                 return addTo.slice(0, -1)
             }
-            arraytoNum(numberSplit)
-            if (calculate === parseInt(userEntry)) {
-                yesOrNo = 'Yes'
-                $('#yes').css('color', 'black')
-                $('#yes').css('background', 'limegreen')
-            } else {
-                yesOrNo = 'No'
-                $('#no').css('background', 'red')
-            }
 
-            $('#outputConsole').html("")
+            function displayYesorNo() {
+                if (calculate === parseInt(userEntry)) {
+                    yesOrNo = 'Yes'
+                    $('#yes').css('color', 'black')
+                    $('#yes').css('background', 'limegreen')
+                    $('#no').addClass('d-none')
+                    $('#yes').removeClass('d-none')
+                } else {
+                    yesOrNo = 'No'
+                    $('#no').css('background', 'red')
+                    $('#yes').addClass('d-none')
+                    $('#no').removeClass('d-none')
+                }
+            }
+            arraytoNum(numberSplit)
+            setTimeout(displayYesorNo, 4000)
+
+            $('#outputConsole').html("").hide()
             $('#outputConsole').append(`
                   <div>Your Number (<em>n</em>): ${userEntry}</div>
                    <div>Number of digits (<em>m</em>): ${numberSplit.length}<br>
                    <div>Calculating: ${arraytoNum(numberSplit)}</div></div>
                    <div>Result = ${calculate}</div>
-                `)
+                `).slideDown(3000)
         })
     })
 })(jQuery);
